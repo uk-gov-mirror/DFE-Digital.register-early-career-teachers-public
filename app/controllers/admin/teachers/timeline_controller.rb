0784bf843bf917118da870cc1944288d166280f6
thenapking <tom.giles@gmail.com>
2026-04-15 13:06:03 +0100

    Add breadcrumbs and missing nav

--- app/controllers/admin/teachers/timeline_controller.rb
+++ app/controllers/admin/teachers/timeline_controller.rb
@@ -1,6 +1,25 @@
-class Admin::Teachers::TimelineController < AdminController
-  def show
-    @teacher = Teacher.find(params[:teacher_id])
-    @events = Events::List.new.for_teacher(@teacher)
+module Admin
+  module Teachers
+    class TimelineController < AdminController
+      layout "full"
+
+      def show
+        teacher = Teacher.find_by(id: params[:teacher_id])
+        @teacher = TeacherPresenter.new(teacher)
+        @events = Events::List.new.for_teacher(@teacher)
+        @navigation_items = helpers.admin_teacher_navigation_items(@teacher, :timeline)
+        @breadcrumbs = teacher_breadcrumbs
+      end
+
+    private
+
+      def teacher_breadcrumbs
+        {
+          "Teachers" => admin_teachers_path(page: params[:page], q: params[:q]),
+          @teacher.full_name => admin_teacher_path(@teacher, page: params[:page], q: params[:q]),
+          "Timeline" => nil
+        }
+      end
+    end
   end
 end
--- spec/views/admin/teachers/timeline/show.html.erb_spec.rb
+++ spec/views/admin/teachers/timeline/show.html.erb_spec.rb
@@ -2,12 +2,17 @@ RSpec.describe "admin/teachers/timeline/show.html.erb" do
   let(:teacher) { FactoryBot.create(:teacher) }
 
   before do
-    assign(:teacher, teacher)
+    assign(:teacher, Admin::TeacherPresenter.new(teacher))
     assign(:events, [])
+    assign(:breadcrumbs, {
+      "Teachers" => admin_teachers_path,
+      "Floella Benjamin" => admin_teacher_path(teacher),
+      "Timeline" => nil
+    })
     render
   end
 
-  it "includes a back link to the teacher page" do
-    expect(view.content_for(:backlink_or_breadcrumb)).to have_link("Back", href: admin_teacher_path(teacher))
+  it "includes a breadcrumb to admin teachers list" do
+    expect(view.content_for(:backlink_or_breadcrumb)).to have_link("Teachers", href: admin_teachers_path)
   end
 end
